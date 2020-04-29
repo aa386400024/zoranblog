6138147d0b9003a82205b0f19ef1bee28963ea0c
zoran <41391477@qq.com>
2020-04-29 22:50:05 +0800

    修改回来

--- routes/banner.js
+++ routes/banner.js
@@ -30,9 +30,11 @@ router.post('/add', async(req, res, next) => {
 // 轮播图上传接口
 // upload.single单文件上传
 router.post('/upload', bannerUpload.single('banner_img'), async(req, res, next) => {
-  console.log(req.file); // 前端上传的文件的信息
+  console.log(req.file, '前端上的文件'); // 前端上传的文件的信息
   // .split('public')[1]通过这个方法，把路径从public中间截开，1代表public后面的内容
   let imgPath = req.file.path.split('public')[1]
+  console.log(imgPath);
+  
   // 将截取的路径和服务器拼接
   let imgUrl = 'http://112.126.63.123:3000' + imgPath
   // 发送给前端
@@ -63,7 +65,7 @@ router.post('/update', async(req, res, next) => {
     let user = await querySql(userSql, [username])
     // 获取到user_id,也就是对应作者的ID
     let user_id = user[0].id
-    console.log(user_id)
+    // console.log(user_id)
     let sql = 'update banner set title = ?, banner_img = ?, banner_href = ? where id = ?'
     // 在数据库中查找banner,
     let result = await querySql(sql, [title, banner_img, banner_href, banner_id])
@@ -85,7 +87,7 @@ router.get('/getlist', async(req, res, next) => {
     // 在数据库中查找article,找到user_id对应用户
     let result = await querySql(sql, [banner_id])
     // 将result里面的数据返回给前端
-    console.log(result[0]);
+    // console.log(result[0]);
     res.send({ code: 0, msg: '获取成功', data: result[0] })
   } catch (e) {
     console.log(e);
@@ -109,7 +111,7 @@ router.post('/delete', async(req, res, next) => {
     // sql, [banner_id, user_id]和前端传的内容做一下比对
     let result = await querySql(sql, [banner_id])
     // 将result里面的数据返回给前端
-    console.log(result[0]);
+    // console.log(result[0]);
     res.send({ code: 0, msg: '删除成功', data: null })
   }catch(e) {
     console.log(e);
